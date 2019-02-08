ab7bcb22b9dd9e9bcb0e0adac6e01e0c353df5b0
jzrlza <parinvut12345@gmail.com>
2019-02-08 09:30:29 +0700

    Updated models to store record data.

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from rest_framework import serializers
 from django.contrib import auth
 from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import datetime 
 
 class Message(models.Model):
     subject = models.CharField(max_length=200)
@@ -26,4 +27,22 @@ class Plant(models.Model):
     max_fertility = models.DecimalField(max_digits=10, decimal_places=2, default=100,validators=[MaxValueValidator(100), MinValueValidator(0)])
     def __str__(self):
         return self.name
+
+class SoilProfile(models.Model):
+    owner = models.ForeignKey(auth.models.User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    location =models.CharField(max_length=256)
+    def __str__(self):
+        user = auth.models.User.objects.get(pk=self.owner.pk)
+        return user.username + " - " + self.name
+    
+class SensorRecord(models.Model):
+    soil_profile = models.ForeignKey(SoilProfile, on_delete=models.CASCADE)
+    record_time = models.DateTimeField(default=datetime.now, blank=True)
+    moist = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    ph = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    fertility = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    def __str__(self):
+        soil = SoilProfile.objects.get(pk=self.soil_profile.pk)
+        return soil.name + " - " + self.name
 
