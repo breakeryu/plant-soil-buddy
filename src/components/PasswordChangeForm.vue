deeea4397ff593c160f63f6402b0aadb51b0e5b7
jzrlza <parinvut12345@gmail.com>
2019-03-17 11:35:46 +0700

    Password change page now displays username.

--- src/components/PasswordChangeForm.vue
+++ src/components/PasswordChangeForm.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h3>User: {{ username }}</h3>
+    <br>
     <h3 class="warning" v-if="failed">{{ failed_msg }}</h3>
     <form class="form-regis" v-on:submit.prevent="changePassword">
       <h3>Old Password</h3>
